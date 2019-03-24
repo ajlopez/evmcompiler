cdc3246606e8a35ee17e516e2bfbaab388cda2b0
ajlopez <webmaster@ajlopez.com>
2019-03-24 17:55:38 -0300

    Compile value

--- test/compilers.js
+++ test/compilers.js
@@ -1,6 +1,62 @@
 
 const compilers = require('../lib/compilers');
 
+exports['compile one byte value'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value(1);
+    
+    test.equal(compiler.bytecodes(), '6001');
+};
+
+exports['compile two bytes value'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value(256);
+    
+    test.equal(compiler.bytecodes(), '610100');
+};
+
+exports['compile three bytes value'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value(256 * 256);
+    
+    test.equal(compiler.bytecodes(), '62010000');
+};
+
+exports['compile four bytes value'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value(256 * 256 * 256);
+    
+    test.equal(compiler.bytecodes(), '6301000000');
+};
+
+exports['compile hexa string value'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value('010203040506');
+    
+    test.equal(compiler.bytecodes(), '65010203040506');
+};
+
+exports['compile hexa string value with odd number of digits'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value('10203040506');
+    
+    test.equal(compiler.bytecodes(), '65010203040506');
+};
+
+exports['compile hexa string value with hex prefix'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.value('0x10203040506');
+    
+    test.equal(compiler.bytecodes(), '65010203040506');
+};
+
 exports['compile load storage'] = function (test) {
     const compiler = compilers.compiler();
     
