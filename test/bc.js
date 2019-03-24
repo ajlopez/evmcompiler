048cf41b4d369763d47c51e0ddb26d1bcffd1ec9
ajlopez <webmaster@ajlopez.com>
2019-03-24 16:47:54 -0300

    Compile add, mul, sub, div

--- test/bc.js
+++ test/bc.js
@@ -8,3 +8,14 @@ exports['compile stop'] = function (test) {
         
     test.equal(result, '00');
 };
+
+exports['compile add, mul, sub, div'] = function (test) {
+    const result = bc.compiler()
+        .add()
+        .mul()
+        .sub()
+        .div()
+        .bytecodes();
+        
+    test.equal(result, '01020304');
+};
