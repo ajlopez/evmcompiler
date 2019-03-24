f859db286b854782de39eca5760585cb2aa5f567
ajlopez <webmaster@ajlopez.com>
2019-03-24 20:08:59 -0300

    Compile add numbers

---
+++ test/binaries.js
@@ -0,0 +1,12 @@
+
+const compilers = require('../lib/compilers');
+const geast = require('geast');
+
+exports['compile add numbers'] = function (test) {
+    const compiler = compilers.compiler();
+    const constant = geast.binary('+', geast.constant(42), geast.constant(1));
+    
+    compiler.process(constant);
+    
+    test.equal(compiler.bytecodes(), '602a600101');
+};
