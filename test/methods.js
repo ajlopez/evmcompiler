3454cc7d9adc3f93ce326dd32acd4bf5f9650649
ajlopez <webmaster@ajlopez.com>
2019-03-29 19:57:12 -0300

    Process method, building function signature and hash, first implementation

--- test/methods.js
+++ test/methods.js
@@ -1,5 +1,6 @@
 
 const compilers = require('../lib/compilers');
+const keccak = require('../lib/sha3').keccak_256;
 const geast = require('geast');
 
 geast.node('method', [ 'name', 'type', 'visibility', 'arguments', 'body' ]);
@@ -11,5 +12,14 @@ exports['process empty method'] = function (test) {
     compiler.process(node);
     
     test.equal(compiler.bytecodes(), "5b60006000f3");
+    
+    const result = compiler.fns();
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    
+    test.equal(result[0].signature, "foo()");
+    test.equal(result[0].hash, keccak("foo()").substring(0, 8));
 }
 
