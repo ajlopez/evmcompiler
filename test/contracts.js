6830b4fbf7a8004b41e46f623470cc165f46634c
ajlopez <webmaster@ajlopez.com>
2019-03-30 09:59:37 -0300

    Process contract with method, first implementation

---
+++ test/contracts.js
@@ -0,0 +1,30 @@
+
+const compilers = require('../lib/compilers');
+const keccak = require('../lib/sha3').keccak_256;
+const geast = require('geast');
+
+geast.node('method', [ 'name', 'type', 'visibility', 'arguments', 'body' ]);
+geast.node('contract', [ 'name', 'body' ]);
+
+exports['process contract with empty method'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const node = geast.contract('Counter',
+        geast.sequence([
+            geast.method('foo', 'void', 'public', [], geast.return())
+        ]));
+        
+    compiler.process(node);
+    
+    test.equal(compiler.bytecodes(), "5b60006000f3");
+    
+    const result = compiler.fns();
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    
+    test.equal(result[0].signature, "foo()");
+    test.equal(result[0].hash, keccak("foo()").substring(0, 8));
+}
+
