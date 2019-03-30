cb5fae6b3e9b6fd4d52327fa03463db90595cee1
ajlopez <webmaster@ajlopez.com>
2019-03-30 12:12:12 -0300

    Process contract with one variable

--- test/contracts.js
+++ test/contracts.js
@@ -26,5 +26,33 @@ exports['process contract with empty method'] = function (test) {
     
     test.equal(result[0].signature, "foo()");
     test.equal(result[0].hash, keccak("foo()").substring(0, 8));
+    
+    test.ok(compiler.context());
 }
 
+exports['process contract with variable declaration'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const node = geast.contract('Counter',
+        geast.sequence([
+            geast.variable('counter', 'uint')
+        ]));
+        
+    compiler.process(node);
+    
+    const result = compiler.fns();
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+    
+    const context = compiler.context();
+    
+    test.ok(context);
+    
+    const counter = context.get('counter');
+    
+    test.ok(counter);
+    test.equal(counter.scope, 'contract');
+    test.equal(counter.type, 'uint256');
+}
