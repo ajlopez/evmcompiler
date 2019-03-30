31aaefbad886ca47a49d70154a93756740b64450
ajlopez <webmaster@ajlopez.com>
2019-03-30 10:14:52 -0300

    Context set and get value

--- test/contexts.js
+++ test/contexts.js
@@ -7,3 +7,17 @@ exports['create context as object'] = function (test) {
     test.ok(context);
     test.equal(typeof context, 'object');
 };
+
+exports['get unknown value'] = function (test) {
+    const context = contexts.context();
+    
+    test.equal(context.get('foo'), null);
+};
+
+exports['set and get value'] = function (test) {
+    const context = contexts.context();
+    
+    context.set('answer', 42);
+    
+    test.equal(context.get('answer'), 42);
+};
