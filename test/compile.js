dbe19b33b7963029c286e2f469c0f311678948f5
ajlopez <webmaster@ajlopez.com>
2019-03-31 17:50:48 -0300

    Compile method

---
+++ test/compile.js
@@ -0,0 +1,18 @@
+
+const evmcompiler = require('..');
+const geast = require('geast');
+
+geast.node('method', [ 'name', 'type', 'visibility', 'arguments', 'body' ]);
+geast.node('contract', [ 'name', 'body' ]);
+
+exports['compile simple contract'] = function (test) {
+    const node = geast.contract('Counter',
+        geast.sequence([
+            geast.method('foo', 'void', 'public', [], geast.sequence([]))
+        ]));
+
+    const bytecodes = evmcompiler.compile(node);
+    
+    test.ok(bytecodes);
+    test.equal(typeof bytecodes, 'string');
+};
