d7846acc354bf0a10f973df2d28a1302c4972e8b
ajlopez <webmaster@ajlopez.com>
2019-04-01 18:34:55 -0300

    Additional test, run method with argument

--- test/runcontracts.js
+++ test/runcontracts.js
@@ -7,7 +7,7 @@ const VM = require('ethereumjs-vm');
 geast.node('method', [ 'name', 'type', 'visibility', 'arguments', 'body' ]);
 geast.node('contract', [ 'name', 'body' ]);
 
-exports['process contract with empty method'] = function (test) {
+exports['run contract with empty method'] = function (test) {
     const compiler = compilers.compiler();
     
     const node = geast.contract('Counter',
@@ -29,6 +29,36 @@ exports['process contract with empty method'] = function (test) {
         test.ok(data);
         test.ok(data.return);
         test.equal(data.return.length, 0);
+        test.done();
+    });
+}
+
+exports['run contract with variable declaration and method returning variable'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const node = geast.contract('Counter',
+        geast.sequence([
+            geast.variable('counter', 'uint'),
+            geast.method('getCounter', 'uint', 'public', [], 
+                geast.return(geast.name('counter'))
+            )
+        ]));
+        
+    compiler.process(node);
+
+    const code = compiler.bytecodes();
+    const vm = new VM();
+    const bytes = Buffer.from(code, 'hex');
+    const data = Buffer.from(keccak("getCounter()").substring(0, 8), 'hex');
+
+    test.async();
+    
+    vm.runCode({ code: bytes, data: data, gasLimit: 30000000 }, function (err, data) {
+        test.ok(!err);
+        test.ok(data);
+        test.ok(data.return);
+        test.equal(data.return.length, 32);
+        test.equal(parseInt(data.return.toString('hex'), 16), 0);
         test.done();
     });
 }
