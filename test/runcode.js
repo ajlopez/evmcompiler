a44d8dd1fde1ae1f0641bd3300814956862610dc
ajlopez <webmaster@ajlopez.com>
2019-04-02 15:53:17 -0300

    Process break, continue commands

--- test/runcode.js
+++ test/runcode.js
@@ -118,3 +118,75 @@ exports['run loop command using local variable'] = function (test) {
     });
 };
 
+exports['run loop command with break'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.context(contexts.context('method'));
+    compiler.enterfn(0);
+    
+    const node = geast.sequence([
+        geast.variable('k', 'uint'),
+        geast.loop(
+            geast.binary('<', geast.name('k'), geast.constant(42)),
+            geast.break()
+        ),
+        geast.return(geast.name('k'))
+    ]);
+    
+    compiler.process(node);
+    
+    const code = compiler.bytecodes();
+    const vm = new VM();
+    const bytes = Buffer.from(code, 'hex');
+
+    test.async();
+    
+    vm.runCode({ code: bytes, gasLimit: 30000000 }, function (err, data) {
+        test.ok(!err);
+        test.ok(data);
+        test.ok(data.return);
+        test.equal(data.return.length, 32);
+        test.equal(parseInt(data.return.toString('hex'), 16), 0);
+        test.done();
+    });
+};
+
+exports['run loop command with continue'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.context(contexts.context('method'));
+    compiler.enterfn(0);
+    
+    const node = geast.sequence([
+        geast.variable('k', 'uint'),
+        geast.loop(
+            geast.binary('<', geast.name('k'), geast.constant(42)),
+            geast.sequence([
+                geast.assign(
+                    geast.name('k'),
+                    geast.constant(100)
+                ),
+                geast.continue()
+            ])
+        ),
+        geast.return(geast.name('k'))
+    ]);
+    
+    compiler.process(node);
+    
+    const code = compiler.bytecodes();
+    const vm = new VM();
+    const bytes = Buffer.from(code, 'hex');
+
+    test.async();
+    
+    vm.runCode({ code: bytes, gasLimit: 30000000 }, function (err, data) {
+        test.ok(!err);
+        test.ok(data);
+        test.ok(data.return);
+        test.equal(data.return.length, 32);
+        test.equal(parseInt(data.return.toString('hex'), 16), 100);
+        test.done();
+    });
+};
+
