c91148b5f422b67cb9f3f4f65b8a3f932e2c9aca
ajlopez <webmaster@ajlopez.com>
2019-04-02 12:28:26 -0300

    Add variables to context

--- test/contexts.js
+++ test/contexts.js
@@ -17,6 +17,23 @@ exports['create context with scope'] = function (test) {
     test.equal(context.scope(), 'contract');
 };
 
+exports['add variable to contract context'] = function (test) {
+    const context = contexts.context('contract');
+
+    test.ok(context);
+    test.equal(context.nvars(), 0);
+    
+    context.set('answer', { name: 'answer', type: 'uint' });
+    
+    test.deepEqual(context.get('answer'), { name: 'answer', type: 'uint', scope: 'contract', offset: 0 });
+    test.equal(context.nvars(), 1);
+    
+    context.set('counter', { name: 'counter', type: 'uint' });
+    
+    test.deepEqual(context.get('counter'), { name: 'counter', type: 'uint', scope: 'contract', offset: 1 });
+    test.equal(context.nvars(), 2);
+};
+
 exports['get unknown value'] = function (test) {
     const context = contexts.context();
     
@@ -40,5 +57,20 @@ exports['set and get value in parent context'] = function (test) {
     test.equal(context.get('answer'), 42);
     test.equal(parent.scope(), 'program');
     test.equal(context.scope(), 'contract');
+};
+
+exports['add variables to contract and method'] = function (test) {
+    const parent = contexts.context('contract');
+    const context = contexts.context('method', parent);
+    
+    parent.set('counter', { name: 'counter', type: 'uint' });
+    context.set('k', { name: 'k', type: 'uint' });
+    context.set('j', { name: 'j', type: 'uint' });
+    
+    test.equal(parent.nvars(), 1);
+    test.equal(context.nvars(), 2);
+    test.deepEqual(parent.get('counter'), { name: 'counter', type: 'uint', offset: 0, scope: 'contract' });
+    test.deepEqual(context.get('k'), { name: 'k', type: 'uint', offset: 0, scope: 'method' });
+    test.deepEqual(context.get('j'), { name: 'j', type: 'uint', offset: 1, scope: 'method' });
 };
 
