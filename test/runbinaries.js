7e79b201f1e7708be8df6a78c0f89363a78f6355
ajlopez <webmaster@ajlopez.com>
2019-04-11 09:11:29 -0300

    Additional test, compile and run logical and

--- test/runbinaries.js
+++ test/runbinaries.js
@@ -18,6 +18,12 @@ function runCode(test, code, expected) {
     });
 }
 
+function compileRunCode(test, node, expected) {
+    const compiler = compilers.compiler();
+    compiler.process(node);
+    runCode(test, compiler.bytecodes(), expected);
+}
+
 exports['compile and run add numbers'] = function (test) {
     const compiler = compilers.compiler();
     const constant = geast.binary('+', geast.constant(41), geast.constant(1));
@@ -104,5 +110,12 @@ exports['compile and run xor numbers'] = function (test) {
     test.async();
 
     runCode(test, compiler.bytecodes(), 3);
+};
+
+exports['compile and run logical and'] = function (test) {
+    compileRunCode(test, geast.binary('&&', geast.constant(true), geast.constant(true)), 1);
+    compileRunCode(test, geast.binary('&&', geast.constant(true), geast.constant(false)), 0);
+    compileRunCode(test, geast.binary('&&', geast.constant(false), geast.constant(true)), 0);
+    compileRunCode(test, geast.binary('&&', geast.constant(false), geast.constant(false)), 0);
 };
 
