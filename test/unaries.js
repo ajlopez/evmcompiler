1ca724f49f0da7c3d4ee27f8804313e9cf41de6a
ajlopez <webmaster@ajlopez.com>
2019-04-13 13:36:20 -0300

    Compile logical not

--- test/unaries.js
+++ test/unaries.js
@@ -10,3 +10,14 @@ exports['compile not number'] = function (test) {
     
     test.equal(compiler.bytecodes(), '602a19');
 };
+
+exports['compile logic not'] = function (test) {
+    const compiler = compilers.compiler();
+    const constant = geast.unary('!!', geast.constant(true));
+    
+    compiler.process(constant);
+    
+    test.equal(compiler.bytecodes(), '600161000c57600161000f565b60005b');
+};
+
+
