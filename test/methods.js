f48bade6211ca46ec6b17dea7262c0d715766ed4
ajlopez <webmaster@ajlopez.com>
2019-04-26 11:33:57 -0300

    Load stack argument, WIP

--- test/methods.js
+++ test/methods.js
@@ -14,3 +14,23 @@ exports['process empty method'] = function (test) {
     test.equal(compiler.bytecodes(), "5b00");
 }
 
+exports['process private method that receives an argument and returns value'] = function (test) {
+    const compiler = compilers.compiler();
+    compiler.context(contexts.context());
+    const node = geast.method('foo', 'uint', 'private',
+        [ geast.argument('value', 'uint') ], 
+        geast.sequence([
+            geast.return(
+                geast.binary('+', 
+                    geast.name('value'), 
+                    geast.constant(1))
+            )
+        ])
+    );
+    
+    compiler.process(node);
+    
+    const code = compiler.bytecodes();
+    
+    test.equal(code, "5b80600052506020600f3");
+}
